60866edd4975b6ce142787c17b284454c1ee3213
Felipe Romero <feliperomero3@outlook.com>
2019-04-15 12:32:45 -0600

    Incluir tema 'lumen' de 'bootswatch'.

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Vidly
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/bootstrap-theme.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
